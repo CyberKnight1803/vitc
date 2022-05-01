df760bd9c6acbdead7ef0a51f5145ea38f3ccb0c
CyberKnight1803 <omkarpitale2001@gmail.com>
2022-05-01 10:54:09 +0530

    init

---
+++ main.py
@@ -0,0 +1,14 @@
+import os 
+import argparse 
+
+
+
+def main(args):
+    pass
+
+
+if __name__=="__main__":
+
+    parser = argparse.ArgumentParser()
+    args = parser.parse_args()
+    main(args)
